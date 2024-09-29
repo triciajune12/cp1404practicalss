2b202465495696ace6baa87eba064513bc346e60
triciajune12 <patricia.harris@my.jcu.edu.au>
2024-09-29 23:28:14 +1000

    add functions to score.py

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,15 +1,38 @@
 """
-CP1404/CP5632 - Practical
-Broken program to determine score status
+program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    category = "Invalid score"
-elif score > 90:
-    category = "Excellent"
-elif score > 50:
-    category = "Passable"
-else:
-    category = "Bad"
-print(f"Your score of {score} is {category}")
+
+import random
+
+def main():
+    score = get_score()
+    random_score = random.randint(1, 100)
+    print(f"Your score of {score} is {determine_category(score)}")
+    print(f"Your score of {random_score} is {determine_category(random_score)}")
+
+
+def get_score():
+    score = int(input("Enter score: "))
+    return score
+
+def determine_category(score):
+    if score < 0 or score > 100:
+        category = "Invalid score"
+    elif score >= 90:
+        category = "Excellent"
+    elif score >= 50:
+        category = "Passable"
+    else:
+        category = "Bad"
+    return category
+
+main()
+
+
+
+
+
+
+
+
